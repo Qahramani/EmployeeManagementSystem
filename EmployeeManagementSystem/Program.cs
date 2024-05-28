ac08f8043ceaeb872b2f6cd23519b0c4fce2bc73
testtttttttttt1234 <wapigix358@bsomek.com>
2024-05-28 15:15:15 +0400

    Lists added

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            List<string> name = new List<string>();
+            List<string> surname = new List<string>();
+            List<string> fatherName = new List<string>();
+            List<int> age = new List<int>();
+            List<string> FIN = new List<string>();
+            List<string> phoneNo = new List<string>();
+            List<string> position = new List<string>();
+            List<decimal> salary = new List<decimal>();
         }
     }
 }
